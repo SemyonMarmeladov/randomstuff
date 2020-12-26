4f6c9503a7ebf5436acc45494c73c57601534f68
Semyon <zielinskizielinski@yahoo.com>
2020-12-26 08:12:24 +0100

    reorg

--- transformers/src/Lib.hs
+++ transformers/src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.Identity
+import Control.Monad.Error
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+import Data.Maybe
+import qualified Data.Map as Map
+
+
+exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x"))(Lit 4 `Plus` Lit 2))
+
+type Name = String
+data Exp = Lit Integer 
+         | Var Name
+         | Plus Exp Exp
+         | Abs Name Exp
+         | App Exp Exp 
+         deriving (Show)
+
+data Value = IntVal Integer
+           | FunVal Env Name Exp
+           deriving (Show)
+
+type Env = Map.Map Name Value 
+
+
